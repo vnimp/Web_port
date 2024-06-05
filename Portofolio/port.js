f8dbfe72decc3b36cddf6fd2b81d19d8a9705b5a
vnimp <163292061+vnimp@users.noreply.github.com>
2024-06-05 13:07:26 +0200

    -

--- Portofolio/port.js
+++ Portofolio/port.js
@@ -1,6 +1,12 @@
 
 document.getElementById('form').addEventListener("submit",event=>{
     event.preventDefault();
+//lijst aanmaken compacter
+//alles dynamisch laten verschijnen
+//val (geen nummers of onbestaande namen);
+//form submitten 
+//terugknop 
+//"test" als naam voor id weg
 let name= document.getElementById('naam').value;
 //submit adden aan local storage.
 sessionStorage.setItem("naam",name);
