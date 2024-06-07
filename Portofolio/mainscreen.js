a2a6c7ed87e4019c6bf4b614b0d6817b57a00a22
vnimp <163292061+vnimp@users.noreply.github.com>
2024-06-07 10:10:21 +0200

    added
    added: back button,css animation, flexbox, self executing function+ destructuring

--- Portofolio/mainscreen.js
+++ Portofolio/mainscreen.js
@@ -2,9 +2,12 @@ const url= 'https://the-one-api.dev/v2'
 const api= 'zY4rLuvWizvWKF6bCOXc'
 let goodname=sessionStorage.getItem("naam");
 document.getElementById('welkom').innerHTML=`About: ${goodname}`
+document.getElementById('back').addEventListener('click',function(){
+    window.location.href="../Portofolio/portofolio.html";
+})
 
    //code
-
+//fetch om data op te halen 
     haaldataop(`/character?name=${goodname}`).then(data=>{
         let Char = data.docs[0];
         Displayinfo(Char);
@@ -13,7 +16,7 @@ document.getElementById('welkom').innerHTML=`About: ${goodname}`
 
 
     //functies
-
+//Json manipuleren en weergeven 
     function Displayinfo(Char){
         const info=['birth', 'death', 'gender', 'hair', 'height', 'race', 'realm', 'spouse']
         const lijst= document.getElementById('lijst');
@@ -25,9 +28,11 @@ document.getElementById('welkom').innerHTML=`About: ${goodname}`
         listitem.textContent= info[counter] + ":"+" "+value;
         lijst.appendChild(listitem);
         counter++
-
-        });
+        
+        }); 
     }
+
+    //async en await 
     async function haaldataop(ext){
         const resp= await fetch(`${url}/${ext}`,{
             headers:{
--- Portofolio/port.js
+++ Portofolio/port.js
@@ -1,6 +1,29 @@
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('loaded')
+});
+
 (function(){
-    const message= 'Mellon';
-    console.log('Message');
+    const citizens= ['Humans','Dwarves','Elves','Hobbits'];
+    const[Gondor,Moria,Mirkwood,Hobbiton]=citizens;
+    console.log(Gondor,Moria,Mirkwood,Hobbiton);
+    console.log('Message'); 
+})();
+document.getElementById('ring').addEventListener("mouseover",event=>{
+    event.preventDefault();
+    document.getElementById('1').innerText="One ring to rule them all";
+    document.getElementById('2').innerText="One ring to find them..";
+    document.getElementById('3').innerText="One ring to bring them all..";
+    document.getElementById('4').innerText="And in the darkness bind them";
+    
+    
+})
+
+document.getElementById('ring').addEventListener("mouseleave",event=>{
+    event.preventDefault();
+    document.getElementById('1').innerText="";
+    document.getElementById('2').innerText="";
+    document.getElementById('3').innerText="";
+    document.getElementById('4').innerText="";
 })
 document.getElementById('form').addEventListener("submit",event=>{
     event.preventDefault();
@@ -17,4 +40,6 @@ return
 sessionStorage.setItem("naam",name);
 window.location.href= 'mainscreen.html';
 })
+
+
 
